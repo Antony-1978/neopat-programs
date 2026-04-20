a227fd55e1b503a2804212046ed056d442152c65
Antony-1978 <antonypraveen2246@gmail.com>
2026-04-20 11:32:22 +0530

    Create program12.cpp

---
+++ program12.cpp
@@ -0,0 +1,14 @@
+#include<cmath>
+using namespace std;
+int main()
+{
+    int x,y,z,a,b;
+    cin>>x;
+    cin>>y;
+    z=sqrt(y);
+    a=x*z;
+    b=a*a;
+    cout<<"Area of the largest possible square is "<<b<<"sqcm."<<endl;
+    
+    
+}
